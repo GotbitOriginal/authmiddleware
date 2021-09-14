5ad168a78b3cc065635af2f3095224bdf150f18d
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2021-09-14 18:43:21 +0300

    Init commit

---
+++ authmiddleware.go
@@ -0,0 +1,57 @@
+package authmiddleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/gotbitoriginal/auth/proto/session"
+)
+
+const (
+	AuthStatus = "authStatus"
+	ID         = "id"
+	Login      = "login"
+	Role       = "role"
+)
+
+type AuthMiddleware struct {
+	AuthChecker session.AuthCheckerClient
+}
+
+func NewAuthMiddleware(AuthChecker session.AuthCheckerClient) AuthMiddleware {
+	return AuthMiddleware{
+		AuthChecker: AuthChecker,
+	}
+}
+
+func (am *AuthMiddleware) Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+	
+		tokenHeader := r.Header.Get("Authorization") //Grab the token from the header
+		splitted := strings.Split(tokenHeader, " ")  //The token normally comes in format `Bearer {token-body}`, we check if the retrieved token matched this requirement
+
+		if tokenHeader == "" || len(splitted) != 2 {
+			ctx = context.WithValue(ctx, AuthStatus, false) // nolint:staticcheck
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+
+		token := splitted[1]
+
+		sess, err := am.AuthChecker.Check(context.Background(), &session.Credentials{Token: token})
+		if err != nil {
+			ctx = context.WithValue(ctx, AuthStatus, false) // nolint:staticcheck
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+
+		ctx = context.WithValue(ctx, AuthStatus, true)  // nolint:staticcheck
+		ctx = context.WithValue(ctx, ID, sess.GetId())  // nolint:staticcheck
+		ctx = context.WithValue(ctx, Login, sess.Login) // nolint:staticcheck
+		ctx = context.WithValue(ctx, Role, sess.Role)   // nolint:staticcheck
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gotbitoriginal/authmiddleware
+
+go 1.14
+
+require github.com/gotbitoriginal/auth v0.1.0
